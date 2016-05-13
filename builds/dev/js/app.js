a31fbed9cc8d552db37840dc4c523523b08bcd4d
big-boris <b.g.demchenko@yandex.ru>
2016-05-13 03:54:56 +0300

    Добавлен Выбери микс, хедер и футер контрастом, замена в слайдере на фотораму

--- builds/dev/js/app.js
+++ builds/dev/js/app.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
 
-  $('.bxslider').bxSlider({
-    pager: false,
-    adaptiveHeight: true
-  });
+  // $('.bxslider').bxSlider({
+  //   pager: false,
+  //   adaptiveHeight: true
+  // });
 
   function myScrollTo(o) {
     var t = $("." + o).offset().top - 10;
